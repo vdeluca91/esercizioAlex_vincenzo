cd4bde019cf335ccccf2cd00df91c630f3264a08
vdeluca91 <84314116+vdeluca91@users.noreply.github.com>
2021-06-26 11:55:28 +0200

    configurati i metodi con scope + definite classi derivate

---
+++ vincenzo/2-veicolo.cpp
@@ -0,0 +1,33 @@
+#include "1-veicolo.h"
+#include <iostream>
+using namespace std;
+
+Veicolo::Veicolo(string m, int c):marca(m),cavalli(c){}
+bool Veicolo::operator>(const Veicolo&v)
+{
+	return cavalli > v.cavalli;
+}
+bool Veicolo::operator<(const Veicolo&v)
+{
+	return cavalli < v.cavalli;
+}
+ostream& operator<<(ostream&out, const Veicolo&v)
+{
+	out << "Marca: "<<v.marca<< " - Cavalli: "<<v.cavalli<<endl;
+	return out;
+}
+class Auto:public Veicolo
+{
+	public:
+	Auto(string m,int c):Veicolo(m,c){}
+}
+class Moto:public Veicolo
+{
+	public:
+	Moto(string m,int c):Veicolo(m,c){}
+}
+class Barca:public Veicolo
+{
+	public:
+	Barca(string m,int c):Veicolo(m,c){}
+}
