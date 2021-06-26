afd7a587ba7255e36776f4762047424b0b58b8e4
vdeluca91 <84314116+vdeluca91@users.noreply.github.com>
2021-06-26 17:33:48 +0200

    carico classi albero

--- vincenzo/6-albero.h
+++ vincenzo/6-albero.h
@@ -21,16 +21,25 @@ class Foglia{
         void setPadre(Foglia<H>* padre){this->padre=padre}
         void setValore(Foglia<H>* valore){this->valore=valore;}
 };
-
-
-
-
-
-
-
-
-
-
-
-
+template<class T>
+class Albero
+{
+    private:
+        Foglia<T> * root;
+        string tipo;
+        void inorder(Foglia<T>*p);
+        Foglia<T>* _Min(T x);
+        void trapianta(Foglia<T>*u,Foglia<T>*v);
+    public:
+        Albero(string x);
+        Foglia<T>* getRadice()const; 
+        Foglia<T>* ricerca(int c)const;
+        int altezza(Foglia<T>*)const;
+        void ins(T val);
+        void canc(Foglia<T>* p);
+        void cancPerValore(int c);
+        void stampaLivello(Foglia<T>*, int)const;
+        template <class U> friend
+        ostream& operator<< (ostream& out, const Albero<U>& A);
+};
 #endif
--- vincenzo/7-albero.cpp
+++ vincenzo/7-albero.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 using namespace std;
 
+template<class T> Albero<T>::Albero(string x):tipo(x),root(nullptr){};
 
 
 
+int main(){return 0;}
 
